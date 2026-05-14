0098482643d491cfe74c2abc29f55f156c8359ef
Ruhid Ibadli <ruhid.ibadli@gmail.com>
2026-05-14 16:15:16 +0400

    minor changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -40,6 +40,12 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        navigateFallbackDenylist: [
+          /^\/sitemap\.xml$/,
+          /^\/robots\.txt$/,
+          /^\/google[\w-]*\.html$/,
+          /^\/api\//
+        ],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
